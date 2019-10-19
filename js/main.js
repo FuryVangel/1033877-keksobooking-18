7f9a6eea757f07e69c06669ecc512af52e07a1a1
Veronica Holden <edgecity23@gmail.com>
2019-10-20 01:04:13 +0300

    Личный проект: перламутровые пуговицы

--- js/main.js
+++ js/main.js
@@ -36,32 +36,50 @@
   };
 
   var onSuccess = function (data) {
-    var advertVariants = data;
-
-    for (var i = 0; i < advertVariants.length; i++) {
-      if (advertVariants[i].offer) {
-        mapFragments.appendChild(window.pins.renderPins(advertVariants[i]));
-        mapFragments.childNodes[i].dataset.id = i + 1;
-        mapFragments.childNodes[i].addEventListener('click', function (evt) {
-
-          window.closeCard();
-
-          var id = evt.target.closest('.map__pin').dataset.id;
-          cardsFragment.appendChild(window.renderCard(advertVariants[id - 1]));
-
-          window.map.insertBefore(cardsFragment, document.querySelector('.map__filters-container'));
-
-          var cardCloseButton = document.querySelector('.popup__close');
-          cardCloseButton.addEventListener('mousedown', window.closeCard);
-          document.addEventListener('keydown', function (e) {
-            if (e.keyCode === window.ESC_KEYCODE) {
-              window.closeCard();
-            }
-          });
-        });
+    var advertVariants = data.slice(0, 5);
+    var inputType = document.querySelector('#housing-type');
+    inputType.addEventListener('change', function () {
+      var adFilteredElement = advertVariants.filter(function (advertElement) {
+        if (advertElement.offer.type === inputType.value) {
+          return true;
+        }
+        return inputType.value === 'any';
+      });
+      var pins = document.querySelectorAll('.map__pin');
+      for (var i = 1; i < pins.length; i++) {
+        pins[i].remove();
       }
-    }
-    var pins = document.querySelector('.map__pins');
-    pins.appendChild(mapFragments);
+      drawingPins(adFilteredElement);
+      window.closeCard();
+    });
+
+    var drawingPins = function (adVariants) {
+      for (var i = 0; i < adVariants.length; i++) {
+        if (adVariants[i].offer) {
+          mapFragments.appendChild(window.pins.renderPins(adVariants[i]));
+          mapFragments.childNodes[i].dataset.id = i + 1;
+          mapFragments.childNodes[i].addEventListener('click', function (evt) {
+
+            window.closeCard();
+
+            var id = evt.target.closest('.map__pin').dataset.id;
+            cardsFragment.appendChild(window.renderCard(adVariants[id - 1]));
+
+            window.map.insertBefore(cardsFragment, document.querySelector('.map__filters-container'));
+
+            var cardCloseButton = document.querySelector('.popup__close');
+            cardCloseButton.addEventListener('mousedown', window.closeCard);
+            document.addEventListener('keydown', function (e) {
+              if (e.keyCode === window.ESC_KEYCODE) {
+                window.closeCard();
+              }
+            });
+          });
+        }
+      }
+      var pins = document.querySelector('.map__pins');
+      pins.appendChild(mapFragments);
+    };
+    drawingPins(advertVariants);
   };
 })();
